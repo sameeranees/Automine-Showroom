494723021e7f7315e3ea2f03b9f7c7b782cb5d5f
sameeranees <sj02732@st.habib.edu.pk>
2019-03-25 10:42:54 +0500

    Initial Commit

---
+++ Software Engineering/Software Engineering/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Software_Engineering.Startup))]
+namespace Software_Engineering
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
