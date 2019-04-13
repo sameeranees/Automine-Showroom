ace4ccc67ab4c3cb6fbf0c2187d80e5ee609c3da
sameeranees <sj02732@st.habib.edu.pk>
2019-04-13 17:15:43 +0500

    Charts Created and Added to Home Page

--- Software Engineering/Software Engineering/Controllers/HomeController.cs
+++ Software Engineering/Software Engineering/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Software_Engineering.Models;
+using System.Collections;
+using System.Web.Helpers;
 
 namespace Software_Engineering.Controllers
 {
@@ -11,7 +14,23 @@ namespace Software_Engineering.Controllers
         [Authorize(Roles ="Admin,Finance Manager, Manager")]
         public ActionResult Index()
         {
+            
             return View();
+        }
+        public ActionResult CharterColumn()
+        {
+            var context = new Software_EngineeringEntities1();
+            ArrayList xValue = new ArrayList();
+            ArrayList yValue = new ArrayList();
+            var results = (from c in context.Cars select c);
+            results.ToList().ForEach(rs => xValue.Add(rs.Make));
+            results.ToList().ForEach(rs => yValue.Add(rs.Make.Count()));
+
+            new Chart(width: 600, height: 400, theme: ChartTheme.Green)
+            .AddTitle("Cars")
+            .AddSeries("Default", chartType: "Pie", xValue: xValue, yValues: yValue)
+            .Write();
+            return null;
         }
 
         public ActionResult About()
